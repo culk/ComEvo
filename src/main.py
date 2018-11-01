467abbd9b65e78def1b0ae3ad22f638e8eef96fb
culk <culk@users.noreply.github.com>
2018-11-01 10:06:36 -0700

    Fixed data path for graph loading, added options

--- src/main.py
+++ src/main.py
@@ -5,19 +5,23 @@ import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 
-DATA_DIR = 'data/'
+DATA_DIR = '../data/'
 
-def load_graph():
+def load_graph(version):
     # Loads the stack overflow full network without edge labels
-    path = os.path.join(DATA_DIR, 'stackoverflow_full.txt')
+    if version == 'full':
+        path = os.path.join(DATA_DIR, 'stackoverflow_full.txt')
+    elif version == 'partial':
+        path = os.path.join(DATA_DIR, 'stackoverflow_partial.txt')
     graph = snap.LoadEdgeList(snap.PNEANet, path, 0, 1, ' ')
+    return graph
 
 def print_graph_summary(graph):
     pass
 
 def main():
-    graph = load_graph()
-
+    graph = load_graph('partial')
+    print(graph.GetEdges())
 
 if __name__ == '__main__':
     main()
