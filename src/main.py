e245b3a06e1e393829f56697678a06f8b9619608
culk <culk@users.noreply.github.com>
2018-11-01 09:47:52 -0700

    Added graph loading function

--- src/main.py
+++ src/main.py
@@ -1,17 +1,23 @@
+import os
 import snap
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 
+DATA_DIR = 'data/'
+
 def load_graph():
-    pass
+    # Loads the stack overflow full network without edge labels
+    path = os.path.join(DATA_DIR, 'stackoverflow_full.txt')
+    graph = snap.LoadEdgeList(snap.PNEANet, path, 0, 1, ' ')
 
 def print_graph_summary(graph):
     pass
 
 def main():
-    pass
+    graph = load_graph()
+
 
 if __name__ == '__main__':
     main()
