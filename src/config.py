00eb186613f44943549a748157acefa243d10f41
culk <culk@users.noreply.github.com>
2018-11-02 16:10:28 -0700

    Add first six months edge list as a version

--- src/config.py
+++ src/config.py
@@ -6,6 +6,7 @@ DATA_PATH = {
     'c2q' : '../data/stackoverflow_c2q.txt',
     'c2a' : '../data/stackoverflow_c2a.txt',
     'partial' : '../data/stackoverflow_partial.txt',
+    '6months' : '../data/stackoverflow_first_six_months.txt',
 }
 
 
