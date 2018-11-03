a069538e23afffd31a5e481cc6ab2528d2b47fd3
culk <culk@users.noreply.github.com>
2018-11-02 17:26:29 -0700

    Add first 30 days graph to config

--- src/config.py
+++ src/config.py
@@ -7,6 +7,7 @@ DATA_PATH = {
     'c2a' : '../data/stackoverflow_c2a.txt',
     'partial' : '../data/stackoverflow_partial.txt',
     '6months' : '../data/stackoverflow_first_six_months.txt',
+    '30days' : '../data/stackoverflow_first_30_days.txt',
 }
 
 
